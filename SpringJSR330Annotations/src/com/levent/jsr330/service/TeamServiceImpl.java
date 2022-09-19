71ef70defab54a8d0798d363d14cdbaa37877bfe
levent-yavuz <leventyavuz35@gmail.com>
2022-09-19 23:01:03 +0300

    second commit

--- SpringJSR330Annotations/src/com/levent/jsr330/service/TeamServiceImpl.java
+++ SpringJSR330Annotations/src/com/levent/jsr330/service/TeamServiceImpl.java
@@ -1,9 +1,7 @@
 package com.levent.jsr330.service;
 
+import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.levent.jsr330.dao.TeamDAO;
 import com.levent.jsr330.model.Team;
@@ -11,7 +9,7 @@ import com.levent.jsr330.model.Team;
 @Named
 public class TeamServiceImpl implements TeamService{
 
-	@Autowired
+	@Inject
 	TeamDAO teamDAO;
 
 	public void insertTeam(Team team) {
